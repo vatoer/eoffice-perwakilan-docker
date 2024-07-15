d7249761bd8e71e4cd29352863efa0622982a1d6
vatoer <vatoer.ckplus@gmail.com>
2024-07-15 19:52:11 +0200

    update deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git pull https://github.com/vatoer/eoffice-perwakilan-docker.git
+
+pnpm prisma generate --schema ./prisma/db-edispo/schema.prisma
+pnpm prisma generate --schema ./prisma/db-penomoran/schema.prisma
+
+pnpm build
